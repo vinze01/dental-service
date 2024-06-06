661f269ad82f9df62e2791ed3966fedf192b3a8c
robin.vince01 <robin.vince01@gmail.com>
2024-06-06 19:02:31 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const config = require('./config');
 
 app.use(express.json())
 app.use(cors())
